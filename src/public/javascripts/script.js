b0ba164c58f1aa28a66574242cd1f7419d683823
Vrezh Fedora <vrezhraiden@gmail.com>
2022-11-23 00:38:15 +0100

    in progress

--- src/public/javascripts/script.js
+++ src/public/javascripts/script.js
@@ -9,36 +9,50 @@ var animateButton = function(e) {
     setTimeout(function(){
       e.target.classList.remove('animate');
     },700);
-  };
+};
   
-  var bubblyButtons = document.getElementsByClassName("bubbly-button");
-  
-  for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
-  //////////////////////////
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
 
-  //Modal for booking//
-  var modal = document.getElementById("book_modal");
-  var open_modal = document.getElementById("book_btn");
-  var close_modal = document.getElementById("close_modal");
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+//////////////////////////
 
-  // When the user clicks on the button, open the modal 
+//Modal for booking//
+var modal = document.getElementById("book_modal");
+
+// When the user clicks on the button, open the modal 
+var open_modal = document.getElementById("book_btn");
 open_modal.onmouseup = function() {
-    modal.style.display = "block";
-  }
-
-  // When the user clicks on x, close the modal
+  modal.style.display = "block";
+}
+// When the user clicks on x, close the modal
+var close_modal = document.getElementById("close_modal");
 close_modal.onclick = function() {
   modal.style.display = "none";
 }
 
-  // When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-} 
+}
 
-
-  /////////////////////
+// When the user clicks on the add options, open options 
+var open_options = document.getElementById("open_options");
+open_options.addEventListener("click", function() {
+  this.classList.toggle("active");
+  var options_grid = this.nextElementSibling;
+  if (options_grid.style.maxHeight)
+  {
+    options_grid.style.maxHeight = null;
+    options_grid.style.visibility = "hidden";
+  } 
+  else 
+  {
+    options_grid.style.maxHeight = options_grid.scrollHeight + "px";
+    options_grid.style.visibility = "visible";
+  } 
+});
+/////////////////////
