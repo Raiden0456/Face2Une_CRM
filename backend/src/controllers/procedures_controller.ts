e47e18c12525a2ac0d4bd95e8fc30c15f981175e
Vrezh Fedora <vrezhraiden@gmail.com>
2022-12-27 20:49:58 +0100

    what the fuck
    ne rabotaet fetch

--- backend/src/controllers/procedures_controller.ts
+++ backend/src/controllers/procedures_controller.ts
@@ -9,72 +9,74 @@ export function loadProc(add = 0, res) {
           message:
             err.message || "Some error occurred while retrieving procedures."
         });
-      else if (data.rows.length != 0) {
-        res.json({success: true, data: data.rows});
-      }
-      else {
-        res.json({success: false, data: data.rows});
-      }
+        console.log(data);
+        return;
+      // else if (data.rows.length != 0) {
+      //   res.json({success: true, data: data.rows});
+      // }
+      // else {
+      //   res.json({success: false, data: data.rows});
+      // }
     });
 }
 
-// Find a single procedure with an id
-export function findOneProc(id: number, res) {
-  procedure.getProcById(id, (err, data) => {
-    if (err)
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving procedure."
-      });
-    else if (data.rows.length != 0) {
-      res.json({success: true, data: data.rows});
-    }
-    else {
-      res.json({success: false, data: data.rows});
-    }
-  });
-}
+// // Find a single procedure with an id
+// export function findOneProc(id: number, res) {
+//   procedure.getProcById(id, (err, data) => {
+//     if (err)
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while retrieving procedure."
+//       });
+//     else if (data.rows.length != 0) {
+//       res.json({success: true, data: data.rows});
+//     }
+//     else {
+//       res.json({success: false, data: data.rows});
+//     }
+//   });
+// }
 
 
-// Update a procedure identified by the id in the request
-export function updateProc(proc: {id: number, name: string, description: string, price: number, duration: number, additional: number}, res) {
-  procedure.updateProcById(proc, (err, data) => {
-    if (err)
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while updating procedure."
-      });
-    else {
-      res.json({success: true, data: proc});
-    }
-  });
-}
+// // Update a procedure identified by the id in the request
+// export function updateProc(proc: {id: number, name: string, description: string, price: number, duration: number, additional: number}, res) {
+//   procedure.updateProcById(proc, (err, data) => {
+//     if (err)
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while updating procedure."
+//       });
+//     else {
+//       res.json({success: true, data: proc});
+//     }
+//   });
+// }
 
-// Create a procedure 
-export function createProc(proc: {name: string, description: string, price: number, duration: number, additional: number}, res) {
-  procedure.createProc(proc, (err, data) => {
-    if (err)
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while updating procedure."
-      });
-    else {
-      res.json({success: true, data: proc});
-    }
-  });
-}
+// // Create a procedure 
+// export function createProc(proc: {name: string, description: string, price: number, duration: number, additional: number}, res) {
+//   procedure.createProc(proc, (err, data) => {
+//     if (err)
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while updating procedure."
+//       });
+//     else {
+//       res.json({success: true, data: proc});
+//     }
+//   });
+// }
 
-// Delete a procedure with the specified id in the request
-export function deleteProc(id: number, res) {
-  procedure.deleteProcById(id, (err, data) => {
-    if (err)
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while deleting procedure."
-      });
-    else {
-      res.json({success: true, data: id});
-    }
-  });
-};
+// // Delete a procedure with the specified id in the request
+// export function deleteProc(id: number, res) {
+//   procedure.deleteProcById(id, (err, data) => {
+//     if (err)
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while deleting procedure."
+//       });
+//     else {
+//       res.json({success: true, data: id});
+//     }
+//   });
+// };
 
--- backend/src/models/db.ts
+++ backend/src/models/db.ts
@@ -1,17 +1,24 @@
-import pg from "pg";
-const { Client } = pg;
+// import pg from "pg";
+// const { Client } = pg;
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432
-})
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-export default client;
+// const client = new Client({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: 5432
+// })
+// client.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+// export default client;
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://gzydevicrlzlwvldeuex.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
