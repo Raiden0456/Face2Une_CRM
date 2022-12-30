5d47cdaa4d96eacc3b4af3affc6795a3189d53b9
Vrezh Fedora <vrezhraiden@gmail.com>
2022-12-30 22:08:32 +0100

    build stuff

---
+++ backend/dist/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json' assert { type: "json" };
+import bodyParser from 'body-parser';
+var app = express();
+// Importing the routes //
+import ProceduresRouter from './routes/procedures_route.js';
+import UsersRouter from './routes/users_route.js';
+//////////////////////////
+// middleware //
+app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+////////////////
+// Routing //
+app.use('/', ProceduresRouter);
+app.use('/', UsersRouter);
+/////////////
+export default app;
+//# sourceMappingURL=app.js.map
