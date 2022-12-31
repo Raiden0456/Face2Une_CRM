869c000038e4671de4321123ffec3793ec960e46
Vrezh Fedora <vrezhraiden@gmail.com>
2022-12-31 21:56:34 +0100

    Rendered procedures on react
    but got errors, review needed!!

--- frontend/src/components/base/BookingBox.tsx
+++ frontend/src/components/base/BookingBox.tsx
@@ -8,16 +8,17 @@ import s from './BookingBox.scss';
 interface IBookingBox {
   width?: string;
   type?: 'main' | 'modal';
+  procedure?: object;
 }
 
-const BookingBox: React.FC<IBookingBox> = ({ width = '100%', type = 'main' }) => {
+const BookingBox: React.FC<IBookingBox> = ({ width = '100%', type = 'main', procedure}) => {  
   return (
     <div className={type === 'main' ? s.BookingBox : s.BookingBoxModal} style={{ width: width }}>
       <div className={s.BookingBox__header}>
         <div className={s.BookingBox__header_column}>
           {ModalStore.modalStatus.open && <h2>Your Cart:</h2>}
-          <h3>Back to basics</h3>
-          <p>30 minutes @ 55$</p>
+          <h3>{procedure.name}</h3>
+          <p>{procedure.duration} minutes @ {procedure.price}$</p>
         </div>
         <ButtonContained
           width="15%"
@@ -33,9 +34,7 @@ const BookingBox: React.FC<IBookingBox> = ({ width = '100%', type = 'main' }) =>
       </div>
       <div className={s.BookingBox__content}>
         <p>
-          The Signature manual sculpting technique, suitable for anyone. The basic need to sustain youthful look &amp;
-          plump, radiant skin. / Le modelage manuel signature convient à tout. La base fondamentale pour conserver une
-          appearance jeune et une peau pulpeuse et radieuse.
+        {procedure.description}
         </p>
       </div>
     </div>
