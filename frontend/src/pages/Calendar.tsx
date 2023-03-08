8bf9b4fe59d4da00fbe1d5a9c854aa2a50a6bb9c
Kirill Bondarenko <billionmann@gmail.com>
2023-03-08 15:21:06 +0300

    Calendar flow init

--- frontend/src/pages/Calendar.tsx
+++ frontend/src/pages/Calendar.tsx
@@ -2,29 +2,61 @@ import { ButtonContained } from '../components/base/Button';
 import React, { useState, useEffect } from 'react';
 import { Container } from '../components/base/Container';
 import { ModalStore } from '../store/Modal.store';
-
 import NavBar from '../components/Navbar';
+import { Scheduler } from '@aldabil/react-scheduler';
 
-import s from './Calendar.scss';
+import './Calendar.scss';
 
 export const Calendar = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
   return (
     <Container
       header={<NavBar />}
       width="100%"
       content={
         <>
-          <ButtonContained
-            width="200px"
-            onClick={() => {
-              ModalStore.setModalStatus({
-                action: "addAppointment",
-                open: true,
-              });
+          <div className="AddAppointmentBtn_wrapper">
+            <ButtonContained
+              width="200px"
+              onClick={() => {
+                ModalStore.setModalStatus({
+                  action: 'addAppointment',
+                  open: true,
+                });
+              }}
+            >
+              Add Appointment Manually
+            </ButtonContained>
+          </div>
+
+          <Scheduler
+            view="month"
+            hourFormat="24"
+            loading={loading}
+            onConfirm={(e, action) => {
+              console.log(e, action);
+              return Promise.resolve(e);
             }}
-          >
-            Add Appointment
-          </ButtonContained>
+            onDelete={(id) => {
+              console.log(id);
+              return Promise.resolve(id);
+            }}
+            events={[
+              {
+                event_id: 1,
+                title: 'Event 1',
+                start: new Date('2023/3/10 09:30'),
+                end: new Date('2023/3/10 10:30'),
+              },
+              {
+                event_id: 2,
+                title: 'Event 2',
+                start: new Date('2023/3/8 09:30'),
+                end: new Date('2023/3/8 10:30'),
+              },
+            ]}
+          />
         </>
       }
     />
