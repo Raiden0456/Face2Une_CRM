be3205f7694725f683ae4bbc89d22dafbebd0aae
Vrezh Fedora <vrezhraiden@gmail.com>
2023-03-09 13:23:26 +0100

    details added and date_end

--- backend/src/models/appointments_model.ts
+++ backend/src/models/appointments_model.ts
@@ -16,21 +16,60 @@ const appointment = function (appointment) {
 };
 
 appointment.getAppointments = async (
-  filter: { column: string; value: any } = { column: "", value: false },
+  filter: { column: string; value: any; details: string; } = { column: "", value: false, details: 'false' },
   result
 ) => {
-  var resp;
-  resp = filter.value
-    ? await supabase
-        .from("appointments")
-        .select("*")
-        .eq(filter.column, filter.value)
-        .order("reservation_date", { ascending: true })
-    : await supabase
-        .from("appointments")
-        .select("*")
-        .order("reservation_date", { ascending: true });
+  var resp; 
+  let boolean_details = (filter.details === "true");
+  switch (boolean_details) {
+    case true:
+      resp = filter.value
+      ? await supabase
+          .from("appointments")
+          .select("*")
+          .eq(filter.column, filter.value)
+          .order("reservation_date", { ascending: true })
+      : await supabase
+          .from("appointments")
+          .select("*")
+          .order("reservation_date", { ascending: true });
 
+
+      // add procedure name and additional names to each appointment as well as appointment date end //
+      for (let i = 0; i < resp.data.length; i++) {
+        let { data: proc } = await supabase
+          .from("procedures")
+          .select("name, duration")
+          .eq("id", resp.data[i].procedure_id);
+        resp.data[i].procedure_name = proc[0].name;
+        let duration = proc[0].duration;
+        resp.data[i].additional_names = [];
+        for (let j = 0; j < resp.data[i].additional_ids.length; j++) {
+          let { data: add_proc } = await supabase
+            .from("procedures")
+            .select("name, duration")
+            .eq("id", resp.data[i].additional_ids[j]);
+          resp.data[i].additional_names.push(add_proc[0].name);
+          duration += add_proc[0].duration;
+        }
+        let date_end = new Date(resp.data[i].reservation_date_time);
+        date_end.setMinutes(date_end.getMinutes() + duration);
+        resp.data[i].date_end = date_end;
+      }
+      break;
+    default:
+      resp = filter.value
+      ? await supabase
+          .from("appointments")
+          .select("*")
+          .eq(filter.column, filter.value)
+          .order("reservation_date", { ascending: true })
+      : await supabase
+          .from("appointments")
+          .select("*")
+          .order("reservation_date", { ascending: true });
+      break;
+  }
   return result(resp.error, resp.data);
 };
 
@@ -76,6 +115,7 @@ appointment.createAppoint = async (
         client_id: appoint.client_id,
         total_price: appoint.total_price,
         saloon_id: appoint.saloon_id,
+        reservation_date_time: appoint.reservation_date_time,
       },
     ])
     .select();
@@ -111,6 +151,7 @@ appointment.updateAppointById = async (
         client_id: appoint.client_id,
         total_price: appoint.total_price,
         saloon_id: appoint.saloon_id,
+        reservation_date_time: appoint.reservation_date_time,
       },
     ])
     .eq("id", appoint.id)
