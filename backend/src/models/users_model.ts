76cd5f752737d6b25dbe56c71c8d4d66d121fd9e
Vrezh Fedora <vrezhraiden@gmail.com>
2023-03-16 10:29:35 +0100

    fix metadata

--- backend/src/models/users_model.ts
+++ backend/src/models/users_model.ts
@@ -163,7 +163,7 @@ user.updateUserById = async (
     {
       email: user.email,
       password: user.password,
-      data: {
+      user_metadata: {
         first_name: user.first_name,
         last_name: user.last_name,
         phone: user.phone,
