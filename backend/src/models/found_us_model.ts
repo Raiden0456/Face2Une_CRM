861c51c7f3edeccdac759bbbcdd3d5e71a9c2f20
Vrezh Fedora <vrezhraiden@gmail.com>
2023-04-14 10:21:39 +0200

    found us sources

--- backend/src/models/found_us_model.ts
+++ backend/src/models/found_us_model.ts
@@ -1,91 +1,64 @@
-// import { QueryResult } from "pg";
-// import client from "./db.js";
 import supabase from "./db.js";
 
-// Constructor
-const source = function (source) {
-  this.id = source.id;
-  this.source = source.source;
-  this.weight = source.weight;
-};
-
-source.getAllSources = async (result) => {
-    let { data: sources, error } = await supabase
+class Source {
+  static async getAllSources() {
+    const { data: sources, error } = await supabase
       .from("found_us")
-      .select("id, source")
-    result(error, sources);
-  return result;
-};
+      .select("id, source");
+    if (error) throw error;
+    return sources;
+  }
 
-source.getSourceById = async (id: number, result) => {
-  let { data: sources, error } = await supabase
-    .from("found_us")
-    .select("*")
-    .eq("id", id);
-  return result(error, sources);
-};
+  static async getSourceById(id: number) {
+    const { data: sources, error } = await supabase
+      .from("found_us")
+      .select("*")
+      .eq("id", id);
+    if (error) throw error;
+    return sources;
+  }
 
-source.createSource = async (
-  proc: {
-    source: string;
-  },
-  result
-) => {
-  const { data, error } = await supabase
-    .from("found_us")
-    .insert([
-      {
-        source: proc.source,
-      },
-    ])
-    .select();
-  return result(error, data);
-};
+  static async createSource(source: string) {
+    const { data, error } = await supabase
+      .from("found_us")
+      .insert([{ source }])
+      .select();
+    if (error) throw error;
+    return data;
+  }
 
-source.updateSourceById = async (
-  proc: {
-    id: number;
-    source: string;
-  },
-  result
-) => {
-  const { data, error } = await supabase
-    .from("found_us")
-    .update([
-      {
-        source: proc.source,
-      },
-    ])
-    .eq("id", proc.id)
-    .select();
-  return result(error, data);
-};
+  static async updateSourceById(id: number, source: string) {
+    const { data, error } = await supabase
+      .from("found_us")
+      .update([{ source }])
+      .eq("id", id)
+      .select();
+    if (error) throw error;
+    return data;
+  }
 
-source.deleteSourceById = async (id: number, result) => {
-  const { data, error } = await supabase
-    .from("found_us")
-    .delete()
-    .eq("id", id);
-  return result(error, data);
-};
+  static async deleteSourceById(id: number) {
+    const { data, error } = await supabase
+      .from("found_us")
+      .delete()
+      .eq("id", id);
+    if (error) throw error;
+    return data;
+  }
 
-source.addWeight = async (sourceid: { id: number }, result) => {
-  let { data: source } = await supabase
-    .from("found_us")
-    .select("weight")
-    .eq("id", sourceid.id);
-  const { data, error } = await supabase
-    .from("found_us")
+  static async addWeight(sourceid: { id: number }) {
+    const { data: source } = await supabase
+      .from("found_us")
+      .select("weight")
+      .eq("id", sourceid.id);
+    const { data, error } = await supabase
+      .from("found_us")
+      .update([{ weight: source[0].weight + 1 }])
+      .eq("id", sourceid.id)
+      .select();
+    if (error) throw error;
+    return data;
+  }
+}
 
-    .update([
-      {
-        weight: source[0].weight + 1,
-      },
-    ])
-    .eq("id", sourceid.id)
-    .select();
-  return result(error, data);
-};
-
-
-export default source;
+export default Source;
