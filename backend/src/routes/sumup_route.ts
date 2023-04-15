ea38e2c02fcd3bf94eb6a60232e04b610630c2b4
Vrezh Fedora <vrezhraiden@gmail.com>
2023-04-16 01:41:59 +0200

    test payment cases

--- backend/src/routes/sumup_route.ts
+++ backend/src/routes/sumup_route.ts
@@ -29,9 +29,32 @@ router.post('/webhook', async (req: Request, res: Response) => {
       // Get the updated checkout status from SumUp's API
       const checkoutStatus = await getCheckoutStatus(checkoutId);
 
-      // Update your application based on the new status
+      // Update application based on the new status
+      switch (checkoutStatus) {
+        case 'PENDING':
+          console.log('The payment is pending.');
+          break;
+        case 'AUTHORIZED':
+          console.log('The payment has been authorized.');
+          break;
+        case 'CAPTURED':
+          console.log('The payment has been captured.');
+          break;
+        case 'CANCELED':
+          console.log('The payment has been canceled.');
+          break;
+        case 'EXPIRED':
+          console.log('The payment has expired.');
+          break;
+        case 'FAILED':
+          console.log('The payment has failed.');
+          break;
+        default:
+          console.log('Unknown checkout status.');
+      }
       // (e.g., mark an order as paid, update the user's account, send an email receipt)
     }
+    
 
     // Respond to SumUp with a 200 status code to acknowledge receipt of the event
     res.status(200).send('OK');
